28a05526cda67311e05e8735e41183802403f56e
Rick Knight <rmknigh@uwe.nsa.gov>
2022-10-06 17:37:36 -0400

    Changed from discord.Client to commands.Bot, worked on subclassing.

--- music_bot.py
+++ music_bot.py
@@ -1,60 +1,13 @@
 # Standard library imports.
 import os
-import pprint
+import sys
 
 # Third party imports.
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("DISCORD_GUILD")
-
-pprint.pprint(TOKEN)
-pprint.pprint(GUILD)
-
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-intents.presences = True
-
-client = discord.Client(intents=intents)
-
-
-@client.event
-async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f"{client.user} is connected to the following guild:\n"
-        f"{guild.name}(id: {guild.id})"
-    )
-
-    members = "\n - ".join([member.name for member in guild.members])
-    print(f"Guild Members:\n - {members}")
-
-
-# @client.event
-# async def on_member_join(member):
-#     await member.create_dm()
-#     await member.dm_channel.send(f"Hi {member.name}, you greasy Boglim!")
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == "test":
-        await message.channel.send(f"Shut up {message.author}, you greasy Boglim!")
-
-
-client.run(TOKEN)
-
-
-# client.run(TOKEN)
-
-
 # import requests
-
-
 # def handler(pd: "pipedream"):
 #     token = f'{pd.inputs["youtube_data_api"]["$auth"]["oauth_access_token"]}'
 #     authorization = f"Bearer {token}"
@@ -73,9 +26,64 @@ client.run(TOKEN)
 #     # Export the data for use in future steps
 #     return r.json()
 
+########################################################################################
+#                                  Custom Classes.                                     #
+########################################################################################
+class PuckBotClient(commands.Bot):
+    """Subclass commands.Bot clas."""
 
-# class CustomClient(discord.Client):
-#     async def on_ready(self):
-#         print(f'{self.user} has connected to Discord!')
+    def __init__(self, *args, **kwargs):
+        super(PuckBotClient, self).__init__(*args, **kwargs)
 
-# client = CustomClient()
+    async def on_ready(self) -> None:
+        """Override discord.Client on_ready method.  Called when the client is done
+        preparing the data received from Discord. Usually after login is successful
+        and the Client.guilds and co. are filled up.
+        """
+        guild = discord.utils.get(client.guilds, name=GUILD)
+        print(
+            f"{client.user} is connected to the following guild:\n"
+            f"{guild.name}(id: {guild.id})"
+        )
+
+        members = "\n - ".join([member.name for member in guild.members])
+        print(f"Guild Members:\n - {members}")
+
+    # async def on_member_join(member):
+    #     await member.create_dm()
+    #     await member.dm_channel.send(f"Hi {member.name}, you greasy Boglim!")
+
+    async def on_message(self, message: discord.Message) -> None:
+        """Override discord.Client on_message method.  Called when a Message is received.
+
+        Args:
+            message (discord.Message): The current message.
+        """
+        if message.author == client.user:
+            return
+
+        if message.content == "test":
+            await message.channel.send(f"Shut up {message.author}, you greasy Boglim!")
+
+
+########################################################################################
+#                                 Script entrypoint.                                   #
+########################################################################################
+if __name__ == "__main__":
+    load_dotenv()
+    TOKEN = str(os.getenv("DISCORD_TOKEN"))
+    GUILD = str(os.getenv("DISCORD_GUILD"))
+
+    if not TOKEN:
+        sys.exit("DISCORD_TOKEN missing from .env file.")
+
+    if not GUILD:
+        sys.exit("DISCORD_GUILD missing from .env file.")
+
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.message_content = True
+    intents.presences = True
+
+    client = PuckBotClient(intents=intents)
+    client.run(TOKEN)
