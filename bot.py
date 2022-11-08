3e263041a055a010a8666d3254c9ad3ab128f092
Rick Knight <rmknigh@uwe.nsa.gov>
2022-11-08 18:18:26 -0500

    Renaming.

---
+++ bot.py
@@ -0,0 +1,103 @@
+# Standard library imports.
+import asyncio
+import logging
+from os.path import dirname
+
+# Third party imports.
+import discord
+from music_bot.client import PuckBotClient
+from music_bot.common.utils import (
+    get_config,
+    load_extensions,
+)
+from music_bot.formatter import Formatter
+
+# https://medium.com/pythonland/build-a-discord-bot-in-python-that-plays-music-and-send-gifs-856385e605a1
+
+
+# import requests
+# def handler(pd: "pipedream"):
+#     token = f'{pd.inputs["youtube_data_api"]["$auth"]["oauth_access_token"]}'
+#     authorization = f"Bearer {token}"
+#     headers = {"Authorization": authorization}
+#     r = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers=headers)
+#     # Export the data for use in future steps
+#     return r.json()
+
+
+# import requests
+
+
+# def handler(pd: "pipedream"):
+#     headers = {"Authorization": f'Bot {pd.inputs["discord_bot"]["$auth"]["bot_token"]}'}
+#     r = requests.get("https://discord.com/api/users/@me", headers=headers)
+#     # Export the data for use in future steps
+#     return r.json()
+
+########################################################################################
+
+
+########################################################################################
+#                                 Script entrypoint.                                   #
+########################################################################################
+if __name__ == "__main__":
+    # Get all config and environment variables.
+    config = get_config(f"{dirname(__file__)}/.env")
+
+    # Configure logger for the script.
+    logging.getLogger("asyncio").setLevel(logging.ERROR)
+    logging.getLogger("googleapicliet.discovery").setLevel(logging.ERROR)
+    logging.getLogger("googleapicliet.discovery_cache").setLevel(logging.ERROR)
+    logger = logging.getLogger()
+    logger.setLevel(config["log_level"])
+    streamHandler = logging.StreamHandler()
+    streamHandler.setLevel(config["log_level"])
+    streamHandler.setFormatter(Formatter())
+    logger.addHandler(streamHandler)
+
+    # Initialize intents for Discord.
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.message_content = True
+    intents.presences = True
+
+    # Create and run the Discord client.
+    bot = PuckBotClient(
+        command_prefix=config["command_prefix"],
+        description="A music bot for you dumb chuds.",
+        intents=intents,
+    )
+    bot.config = config
+    bot.logger = logger
+
+    async def main():
+        async with bot:
+            # bot.loop.create_task(background_task())
+            await load_extensions(bot, f"{dirname(__file__)}/music_bot/cogs")
+            # await bot.start(config["token"])
+
+    asyncio.run(main())
+# TODO: readme  https://gist.github.com/cyphunk/dfceef02f5ad7b20df6b389aa777ec87
+# await setup(bot)
+# bot.run(config["token"])
+
+# youtube = googleapiclient.discovery.build(
+#     config["api_service_name"],
+#     config["api_version"],
+#     developerKey=config["developer_key"],
+# )
+
+# 'request' variable is the only thing you must change
+# depending on the resource and method you need to use
+# in your query
+# playlist_response = youtube.playlistItems().list(
+#     part="snippet,contentDetails,id,status", playlistId=str(os.getenv("GAME_NIGHT"))
+# )
+
+# Query execution
+# response = playlist_response.execute()
+# Print the results
+# Standard library imports.
+# import pprint
+
+# pprint.pprint(response)
